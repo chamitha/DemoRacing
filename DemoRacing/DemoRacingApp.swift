c8a088ad0db62a82b5feefd075be8769ed89b6ef
Chamitha Wijesekera <chamitha.wijesekera@service.vic.gov.au>
2024-10-23 10:34:13 +1100

    Initial commit

---
+++ DemoRacing/DemoRacingApp.swift
@@ -0,0 +1,17 @@
+//
+//  DemoRacingApp.swift
+//  DemoRacing
+//
+//  Created by Chamitha Wijesekera on 23/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct DemoRacingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ DemoRacingTests/DemoRacingTests.swift
@@ -0,0 +1,17 @@
+//
+//  DemoRacingTests.swift
+//  DemoRacingTests
+//
+//  Created by Chamitha Wijesekera on 23/10/2024.
+//
+
+import Testing
+@testable import DemoRacing
+
+struct DemoRacingTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
